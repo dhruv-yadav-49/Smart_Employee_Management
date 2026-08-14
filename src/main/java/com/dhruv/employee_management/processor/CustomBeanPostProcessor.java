07779e353a9d41e32ee8bf0393e69536ae72030b
dhruv-yadav-49 <dhruvofficial73@gmail.com>
2026-08-14 23:54:23 +0530

    feat: add CustomBeanPostProcessor to observe bean lifecycle

---
+++ src/main/java/com/dhruv/employee_management/processor/CustomBeanPostProcessor.java
@@ -0,0 +1,21 @@
+package com.dhruv.employee_management.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Before Initialization -> " + beanName);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("After Initialization -> " + beanName);
+        return bean;
+    }
+}
